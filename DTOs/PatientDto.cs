2cf4611d5ed7ace175cee387ce46ed77632f7667
aimenyusra <aimenbather@gmail.com>
2026-07-17 21:03:36 +0530

    added validation for dtos

--- DTOs/PatientDto.cs
+++ DTOs/PatientDto.cs
@@ -1,8 +1,16 @@
-﻿namespace Hospital.DTOs
+﻿using Hospital.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hospital.DTOs
 {
     public class PatientDto
     {
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(1,120)]
+        [MinimumAge]
         public int Age { get; set; }
         public string Disease { get; set; } = string.Empty;
     }
---
+++ Validation/MinimumAgeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital.Validation
+{
+    public class MinimumAgeAttribute:ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is int age)
+            {
+                if (age < 18)
+                {
+                    return new ValidationResult("Age must be at least 18.");
+                }
+                else
+                {
+                    return ValidationResult.Success!;
+                }
+
+            }
+            else
+            {
+                return new ValidationResult("Invalid age value.");
+
+            }
+    }
+}
