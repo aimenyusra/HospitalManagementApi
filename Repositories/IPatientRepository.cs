c949f15ea7aee0609061b56b0c85f376dd13cda4
aimenyusra <aimenbather@gmail.com>
2026-07-29 16:54:10 +0530

    added generic repository

--- Repositories/IPatientRepository.cs
+++ Repositories/IPatientRepository.cs
@@ -3,9 +3,13 @@ using Hospital.Models;
 
 namespace Hospital.Repositories
 {
-    public interface IPatientRepository : IGenericRepository<Patient>
+    public interface IPatientRepository
     {
-  
+        Task<IEnumerable<Patient>> GetPatientsAsync();
         Task<IEnumerable<Patient>> SearchPatientsAsync(string search);
+        Task<Patient?> GetPatientByIdAsync(int id);
+        Task<Patient> AddPatientAsync(Patient patient);
+        Task<Patient?> UpdatePatientAsync( Patient patient);
+        Task<bool> DeletePatientAsync(Patient patient);
     }
 }
